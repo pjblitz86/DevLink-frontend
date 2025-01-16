09470677575026966987667cfcff59b57837fb95
pjblitz86 <paulius.juknis@gmail.com>
2025-01-16 08:57:18 +0200

    global styles, initial Navbar and Landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+import './App.css';
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <Navbar />
+      <Landing />
+    </>
+  );
 };
 
 export default App;
---
+++ src/components/layout/Navbar.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Navbar = (props) => {
+  return (
+    <nav className='navbar bg-dark'>
+      <h1>
+        <a href='index.html'>
+          <i className='fas fa-code'></i> DevLink
+        </a>
+      </h1>
+      <ul>
+        <li>
+          <a href='profiles.html'>Developers</a>
+        </li>
+        <li>
+          <a href='register.html'>Register</a>
+        </li>
+        <li>
+          <a href='login.html'>Login</a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+Navbar.propTypes = {};
+
+export default Navbar;
