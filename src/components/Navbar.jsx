6d486e110ccf30929ba6850657825d68b45d6e36
pjblitz86 <paulius.juknis@gmail.com>
2025-01-22 11:02:49 +0200

    logout and navbar links logic

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,31 +1,63 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { logout } from '../features/authSlice';
 
 const Navbar = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
+
+  const handleLogout = () => {
+    dispatch(logout());
+    navigate('/');
+  };
+
+  const authLinks = (
+    <ul>
+      <li>
+        <Link to='/profiles'>Developers</Link>
+      </li>
+      <li>
+        <Link to='/posts'>Posts</Link>
+      </li>
+      <li>
+        <Link to='/dashboard'>
+          <i className='fas fa-user' />{' '}
+          <span className='hide-sm'>Dashboard</span>
+        </Link>
+      </li>
+      <li>
+        <a onClick={handleLogout} href='#!'>
+          <i className='fas fa-sign-out-alt' />{' '}
+          <span className='hide-sm'>Logout</span>
+        </a>
+      </li>
+    </ul>
+  );
+
+  const guestLinks = (
+    <ul>
+      <li>
+        <Link to='/profiles'>Developers</Link>
+      </li>
+      <li>
+        <Link to='/register'>Register</Link>
+      </li>
+      <li>
+        <Link to='/login'>Login</Link>
+      </li>
+    </ul>
+  );
+
   return (
     <nav className='navbar bg-dark'>
       <h1>
-        <NavLink to='/'>
-          <i className='fas fa-code'></i> DevLink
-        </NavLink>
+        <Link to='/'>
+          <i className='fas fa-code' /> DevConnector
+        </Link>
       </h1>
-      <ul>
-        <li>
-          <NavLink to='/profiles'>Developers</NavLink>
-        </li>
-        <li>
-          <NavLink to='/dashboard'>
-            <i className='fas fa-user' />{' '}
-            <span className='hide-sm'>Dashboard</span>
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='/register'>Register</NavLink>
-        </li>
-        <li>
-          <NavLink to='/login'>Login</NavLink>
-        </li>
-      </ul>
+      <>{isAuthenticated ? authLinks : guestLinks}</>
     </nav>
   );
 };
