8365df0e51b021eb7cd75842dac150b6b6c66c72
pjblitz86 <paulius.juknis@gmail.com>
2025-02-11 11:23:56 +0200

    api.js fix

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-const API_URL = import.meta.env.VITE_API_URL || '/api';
+const API_URL = import.meta.env.VITE_API_URL
+  ? `${import.meta.env.VITE_API_URL}/api`
+  : '/api';
 
 const api = axios.create({
   baseURL: API_URL,
