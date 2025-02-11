d58d69a64d18d780f6df562a42776a6895f9b066
pjblitz86 <paulius.juknis@gmail.com>
2025-02-11 09:39:03 +0200

    vite config for production

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,22 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8080',
-        changeOrigin: true,
-        secure: false
+export default defineConfig(({ mode }) => {
+  const isProduction = mode === 'production';
+
+  return {
+    plugins: [react()],
+    server: {
+      port: 3000,
+      proxy: {
+        '/api': {
+          target: isProduction
+            ? 'https://devlink-deploy.onrender.com' // Production API
+            : 'http://localhost:8080', // Development API
+          changeOrigin: true,
+          secure: false
+        }
       }
     }
-  }
+  };
 });
