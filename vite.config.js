6a55da975f7776197309701504b730991945f639
pjblitz86 <paulius.juknis@gmail.com>
2025-02-11 11:06:41 +0200

    vite config changes

--- vite.config.js
+++ vite.config.js
@@ -3,20 +3,26 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
   const isProduction = mode === 'production';
+  const API_URL = isProduction
+    ? 'https://devlink-deploy.onrender.com' // Production API
+    : 'http://localhost:8080'; // Development API
 
   return {
     plugins: [react()],
     server: {
       port: 3000,
-      proxy: {
-        '/api': {
-          target: isProduction
-            ? import.meta.env.VITE_API_URL // Production API
-            : 'http://localhost:8080', // Development API
-          changeOrigin: true,
-          secure: false
-        }
-      }
+      proxy: !isProduction
+        ? {
+            '/api': {
+              target: API_URL,
+              changeOrigin: true,
+              secure: false
+            }
+          }
+        : undefined // No proxy in production
+    },
+    define: {
+      'process.env.API_URL': JSON.stringify(API_URL)
     }
   };
 });
